811905f9241a989348c3dd0fcf14aa23b3c1cb61
Lex Van-Nugent <lexvn.wd@gmail.com>
2023-05-03 17:50:26 -0600

    Fix sizing for item containers

--- src/components/Slideshow.jsx
+++ src/components/Slideshow.jsx
@@ -6,17 +6,17 @@ export default function Slideshow() {
     <section className="h-full w-full">
       <div className="h-full w-full">
         <Carousel slideInterval={3000} className="bg-black w-full h-full">
-          <div className='flex flex-row justify-center h-full w-full'>
-            <img src="/static/products/apparel5.jpg" alt="Apparel 5" className="object-contain h-full w-full"></img>
-            <img src="/static/products/apparel4.jpg" alt="Apparel 4" className="object-contain h-full w-full"></img>
+          <div className='flex flex-row justify-around items-center h-full w-full'>
+            <img src="/static/products/apparel5.jpg" alt="Apparel 5" className="object-fill object-center h-[90%] w-[35%]"></img>
+            <img src="/static/products/apparel4.jpg" alt="Apparel 4" className="object-fill object-center h-[90%] w-[35%]"></img>
           </div>
-          <div className='flex flex-row justify-center h-full w-full'>
-            <img src="/static/products/accessories3.jpg" alt="Accessories 3" className="object-contain h-full w-full"></img>
-            <img src="/static/products/accessories4.jpg" alt="Accessories 4" className="object-contain h-full w-full"></img>
+          <div className='flex flex-row justify-around items-center h-full w-full'>
+            <img src="/static/products/accessories3.jpg" alt="Accessories 3" className="object-fill object-center h-[90%] w-[35%]"></img>
+            <img src="/static/products/accessories4.jpg" alt="Accessories 4" className="object-fill object-center h-[90%] w-[35%]"></img>
           </div>
-          <div className='flex flex-row justify-center h-full w-full'>
-            <img src="/static/products/tools3.jpg" alt="Tools 3" className="object-contain h-full w-full"></img>
-            <img src="/static/products/tools1.jpg" alt="Top-Rated 2" className="object-contain h-full w-full"></img>
+          <div className='flex flex-row justify-around items-center h-full w-full'>
+            <img src="/static/products/tools3.jpg" alt="Tools 3" className="object-fill object-center h-[90%] w-[35%]"></img>
+            <img src="/static/products/tools1.jpg" alt="Top-Rated 2" className="object-fill object-center h-[90%] w-[35%]"></img>
           </div>
         </Carousel>
       </div>

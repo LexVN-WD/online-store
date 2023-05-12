8b5fc57cba3fd0761295e72d058fb20d1be4ff82
Lex Van-Nugent <lexvn.wd@gmail.com>
2023-05-12 16:15:11 -0600

    Create Home Interior endpoint

---
+++ src/pages/categories/homeinterior/index.jsx
@@ -0,0 +1,12 @@
+import NavBar from "../../../components/NavBar";
+
+export default function HomeInterior() {
+  return (
+    <>
+      <div className="h-screen w-screen">
+        <NavBar />
+        <h1>Home Interior</h1>
+      </div>
+    </>
+  );
+}

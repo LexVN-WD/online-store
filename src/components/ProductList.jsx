fd3b9bc5f170ed3c4b663a4587c4247587130bd9
Lex Van-Nugent <lexvn.wd@gmail.com>
2023-05-21 19:44:25 -0600

    Create initial ProductList component

---
+++ src/components/ProductList.jsx
@@ -0,0 +1,11 @@
+export default function ProductList() {
+  return (
+    <>
+    <section className="h-full w-full">
+      <div>
+        <h1>Product List</h1>
+      </div>
+    </section>
+    </>
+  )
+}
